b802c0a2d6063a58134c917b685253f9cbc2cfe7
Berna <104700942+Bernahm@users.noreply.github.com>
2022-07-24 13:12:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,19 @@ const Discord = require('discord.js');
 const config = require('./config/config.json')
 require('colors')
 const client = new Discord.Client({
-    restTimeOffset: 0,
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
     intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
     ],
 })
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
-client.color = config.color;
 
 
 function requerirhandlers() {
-    ["command", "events"].forEach(handler => {
+    ["command", "events", "funciones"].forEach(handler => {
        
         try {
             require(`./handlers/${handler}`)(client, Discord)
@@ -29,5 +23,6 @@ function requerirhandlers() {
         }
     })
 }
+requerirhandlers();
 
 client.login(config.token).catch(() => console.log(`-[X]- NO HAS ESPECIFICADO UN TOKEN VALIDO O TE FALTAN INTENTOS -[X]-\n [-] ACTIVA LOS INTENTOS EN https://discord.dev [-]`.red))
