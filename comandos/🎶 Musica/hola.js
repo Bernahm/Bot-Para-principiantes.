3d37e6c6b829d4308d888c4a47bd834d2a966cf3
Berna <104700942+Bernahm@users.noreply.github.com>
2022-07-30 02:06:02 +0200

    Delete comandos/🎶 Musica directory

--- comandos/🎶 Musica/hola.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "hola",
-    desc: "Sirve para ver la latencia del Bot",
-    run: async (client, message, args, prefix) => {
-        message.reply(`Hola! ${message.author.username}`)
-    }
-}
